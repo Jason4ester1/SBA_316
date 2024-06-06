0c3ee4646902dc4dbbb2422757a89c1565178c56
Jason Forrester <jason4ester1@gmail.com>
2024-06-06 11:04:12 -0500

    added eventlisteners

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-function changeBackground(cream){
-    document.body.style.background = color; 
+const date = 'Time: ' + new Date().getHours() + ':' + new Date().getMinutes()
+const topArea = document.getElementById("top-area")
+
+window.addEventListener("load", (event) => {
+  event.preventDefault();
+  topArea.append(date)
+});
+const options = document.getElementsByTagName("li")
+for(let i = 0;i < options.length; i++ ){
+  console.log(options[i])
+  options[i].addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    options[i].style.color = 'green'
+  })
+
 }
+
+
+function changeBackground(color){
+    document.body.style.backgroundColor = color; 
+}
+
+changeBackground('cream')
 const heading = document.querySelector("#heading")
 
 heading.style.color = "brown"
@@ -13,15 +33,13 @@ function addElement() {
   const newContent = document.createTextNode("Say hi to Doffee the Coffee!");
 
   newDiv.appendChild(newContent);
-
- const currentDiv = document.getElementById("div1");
- document.body.insertBefore(newDiv, currentDiv);
+  const parent = document.getElementById('menu')
+ const currentDiv = document.getElementById("menu-list");
+ parent.insertBefore(newDiv, currentDiv);
 }
 
 const about = document.querySelector("#about")
 const h2 = document.getElementById("h2").innerHTML = "Doffee Coffee"
 about.appendChild.h2
-console.log(h2)
 
-document.getElementById("#").addEventListener("click", displayDate);
   
